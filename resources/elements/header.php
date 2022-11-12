808c901ed11f17b31ec113f727d3cc66efd27a84
InTostor <gena05irk@yandex.ru>
2022-11-12 13:28:21 +0800

    fixed wrong link on header

--- resources/elements/header.php
+++ resources/elements/header.php
@@ -21,7 +21,7 @@ $isUserLogged = User::isLogged();
                 3DExchange</a>
             </div>
             <nav class="header_nav">
-                <?php if($canMakePart){echo'<a class="navlink" id="addPart" href="item?add"><span class="material-icons addPart">post_add</span></a>';} ?>
+                <?php if($canMakePart){echo'<a class="navlink" id="addPart" href="/item?add"><span class="material-icons addPart">post_add</span></a>';} ?>
                 <a class="navlink" id="accLink" href="/account"><span class="material-icons account">account_circle</span></a>
                 <?php if($isUserLogged){echo'<a class="navlink" id="addPart" href="/account/logout.php"><span class="material-icons addPart">logout</span></a>';} ?>
             </nav>
